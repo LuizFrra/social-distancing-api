fa1448b4e239ca832d188bb7d57bc49efec78e88
Luiz Ferreira <luizalex1997@gmail.com>
2021-03-17 07:58:22 -0300

    Removido Teste ao inicializar
    Foi removido o teste ao iniciliazar a aplicação, uma vez que alguns
    serviços como banco de dados e flyway não estão configurados.

--- api/src/test/java/com/luiz/api/TccApplicationTests.java
+++ api/src/test/java/com/luiz/api/TccApplicationTests.java
@@ -1,13 +1,8 @@
 package com.luiz.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TccApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
