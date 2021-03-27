b13e3618e3866e80969e7e275d2518c0d84938ca
Luiz Ferreira <luizalex1997@gmail.com>
2021-03-27 14:41:33 -0300

    Definir a entidade de domínio device e seus serviços
    1. Foi criada a entidade de device e a entidade para representar o status
    do dispositivo.
    2. Foi adicionado as dependência na camada de dominios
    para permitir as annotations utilizadas pelo hibernata e também pelo
    spring boot.
    3. Foi adicicionado a dependência da camada de dominio a camada de API.
    
    \#17

---
+++ domain/src/main/java/com/luiz/domain/entities/device/Device.java
@@ -0,0 +1,72 @@
+package com.luiz.domain.entities.device;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "device")
+public class Device {
+
+    @Id
+    private Long id;
+
+    /**
+     * A unique device identifier like a MAC Address
+     */
+    @Column(unique = true, length = 20, updatable = false, nullable = false)
+    private String identifier;
+
+    /**
+     * A data to identifier the current status of device
+     */
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DeviceStatus status;
+
+    /**
+     * This data will be use to encrypt device communication
+     */
+    @Column(length = 20)
+    private String key;
+
+    public Device() {
+    }
+
+    public Device(Long id, String identifier, DeviceStatus status, String key) {
+        this.id = id;
+        this.identifier = identifier;
+        this.status = status;
+        this.key = key;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public DeviceStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeviceStatus status) {
+        this.status =  status;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
