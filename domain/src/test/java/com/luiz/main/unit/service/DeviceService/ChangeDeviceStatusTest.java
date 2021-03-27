4cb0b3d5ddc4658b58739b73df47af83838401d7
Luiz Ferreira <luizalex1997@gmail.com>
2021-03-27 20:16:41 -0300

    Adicionando testes para o método ChangeDeviceStatus
    1. Adicionado os testes para o método ChangeDeviceStatus validando
    algumas situações de mudanças de status.
    
    \#28

---
+++ domain/src/test/java/com/luiz/main/unit/service/DeviceService/ChangeDeviceStatusTest.java
@@ -0,0 +1,94 @@
+package com.luiz.main.unit.service.DeviceService;
+
+import com.luiz.domain.entities.device.Device;
+import com.luiz.domain.entities.device.DeviceService;
+import com.luiz.domain.entities.device.DeviceServiceImpl;
+import com.luiz.domain.entities.device.DeviceStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@Import(
+        DeviceServiceImpl.class
+)
+public class ChangeDeviceStatusTest {
+
+    @Autowired
+    private DeviceService deviceService;
+
+    @Test
+    public void should_return_false_when_try_change_status_from_blocked_to_online() {
+        Device device = new Device(0L, "123", DeviceStatus.BLOCKED, "123");
+
+        boolean result = deviceService.changeDeviceStatus(device, DeviceStatus.ONLINE);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void should_return_false_when_try_change_status_from_blocked_to_offline() {
+        Device device = new Device(0L, "123", DeviceStatus.BLOCKED, "123");
+
+        boolean result = deviceService.changeDeviceStatus(device, DeviceStatus.OFFLINE);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void should_return_true_when_try_change_status_from_online_to_offline() {
+        Device device = new Device(0L, "123", DeviceStatus.ONLINE, "123");
+
+        boolean result = deviceService.changeDeviceStatus(device, DeviceStatus.OFFLINE);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void should_return_true_when_try_change_status_from_online_to_blocked() {
+        Device device = new Device(0L, "123", DeviceStatus.ONLINE, "123");
+
+        boolean result = deviceService.changeDeviceStatus(device, DeviceStatus.BLOCKED);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void should_return_true_when_try_change_status_from_offline_to_online() {
+        Device device = new Device(0L, "123", DeviceStatus.OFFLINE, "123");
+
+        boolean result = deviceService.changeDeviceStatus(device, DeviceStatus.ONLINE);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void should_return_true_when_try_change_status_from_offline_to_blocked() {
+        Device device = new Device(0L, "123", DeviceStatus.OFFLINE, "123");
+
+        boolean result = deviceService.changeDeviceStatus(device, DeviceStatus.BLOCKED);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void should_return_false_when_try_change_status_from_offline_to_offline() {
+        Device device = new Device(0L, "123", DeviceStatus.OFFLINE, "123");
+
+        boolean result = deviceService.changeDeviceStatus(device, DeviceStatus.OFFLINE);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void should_return_false_when_try_change_status_from_online_to_online() {
+        Device device = new Device(0L, "123", DeviceStatus.ONLINE, "123");
+
+        boolean result = deviceService.changeDeviceStatus(device, DeviceStatus.ONLINE);
+
+        Assertions.assertFalse(result);
+    }
+}
