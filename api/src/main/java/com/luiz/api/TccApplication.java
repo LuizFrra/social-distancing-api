1d21fac22d63e681fbc253e4491f39a8a68b990d
Luiz Ferreira <luizalex1997@gmail.com>
2021-03-27 13:39:55 -0300

    Remover a camada de gateway
    O projeto será feito utilizando boas práticas e seguinte o modelo DDD,
    teremos apenas 3 camadas, a de domínio, infra e a de apresentação que
    será a api.
    
    \#19

--- api/src/main/java/com/luiz/api/TccApplication.java
+++ api/src/main/java/com/luiz/api/TccApplication.java
@@ -3,7 +3,7 @@ package com.luiz.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.luiz")
 public class TccApplication {
 
 	public static void main(String[] args) {
