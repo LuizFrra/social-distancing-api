2cbb113719567e1bda9f3a5cbb5437412fbd725d
Luiz Ferreira <luizalex1997@gmail.com>
2021-03-28 10:08:35 -0300

    Correção erro ao mudar status do dispositivo de offline/online para blocked
    Foi feito a mudança na lógica do método responsável por alterar o estado
    de um dispositivo, a lógica anterior não permitia a mudança do estado
    offline/online para o estado blocked.
    
    \#34

--- domain/src/main/java/com/luiz/domain/entities/device/DeviceServiceImpl.java
+++ domain/src/main/java/com/luiz/domain/entities/device/DeviceServiceImpl.java
@@ -11,22 +11,18 @@ public class DeviceServiceImpl implements DeviceService {
     public boolean changeDeviceStatus(Device device, DeviceStatus status) {
         DeviceStatus currStatus = device.getStatus();
 
-        if (currStatus.equals(DeviceStatus.BLOCKED)) {
+        if (currStatus.equals(DeviceStatus.BLOCKED) || device.getStatus().equals(status)) {
             return false;
-        } else if(currStatus.equals(DeviceStatus.ONLINE) && status.equals(DeviceStatus.OFFLINE)) {
-            device.setStatus(status);
-            return true;
-        } else if(currStatus.equals(DeviceStatus.OFFLINE) && status.equals(DeviceStatus.ONLINE)) {
-            device.setStatus(status);
-            return true;
         }
 
-        return false;
+        device.setStatus(status);
+
+        return true;
     }
 
     @Override
     public boolean createDeviceKey(Device device) {
-        if(device.getKey() == null || device.getKey().isEmpty() || device.getKey().isBlank()) {
+        if (device.getKey() == null || device.getKey().isEmpty() || device.getKey().isBlank()) {
             device.setKey(UUID.randomUUID().toString().replace("-", ""));
             return true;
         }
