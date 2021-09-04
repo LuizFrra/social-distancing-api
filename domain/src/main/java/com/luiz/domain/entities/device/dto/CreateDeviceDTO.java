c903709b67e54d870229177496b497cac8191dfd
Luiz Ferreira <luizalex1997@gmail.com>
2021-09-04 16:23:56 -0300

    fix(device): fix device domain validation bug

--- domain/src/main/java/com/luiz/domain/entities/device/dto/CreateDeviceDTO.java
+++ domain/src/main/java/com/luiz/domain/entities/device/dto/CreateDeviceDTO.java
@@ -1,14 +1,12 @@
 package com.luiz.domain.entities.device.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class CreateDeviceDTO {
 
-    @NotBlank(message = "device.identifier.length")
-    @NotNull(message = "device.identifier.notnull.validation")
-    @Size(message = "device.identifier.length", min = 1, max = 20)
+    @NotBlank(message = "device.identifier.notnull.validation")
+    @Size(message = "device.identifier.length", max = 20)
     public String identifier;
 
     public CreateDeviceDTO() {
