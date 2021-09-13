a9133a8fdd2d6f2ef06621ae995fe6d55b984e9e
Luiz Ferreira <luizalex1997@gmail.com>
2021-09-13 18:39:05 -0300

    refactor(device-controller): split device controller

--- api/src/main/java/com/luiz/api/controller/DeviceEnvController.java
+++ api/src/main/java/com/luiz/api/controller/DeviceEnvController.java
@@ -1,14 +1,10 @@
 package com.luiz.api.controller;
 
 import com.luiz.api.service.DeviceServiceApplication;
-import com.luiz.domain.entities.device.dto.CreateDeviceDTO;
 import com.luiz.domain.entities.device.dto.CreateDeviceEnvDTO;
-import com.luiz.domain.entities.device.dto.CreateDeviceTagDTO;
 import com.luiz.domain.entities.device.dto.DeviceDTO;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,25 +12,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/device")
-public class DeviceController {
+@RequestMapping("/api/v1/device/{deviceId}/env")
+public class DeviceEnvController {
 
     private final DeviceServiceApplication deviceServiceApplication;
 
-    public DeviceController(DeviceServiceApplication deviceServiceApplication) {
+    public DeviceEnvController(DeviceServiceApplication deviceServiceApplication) {
         this.deviceServiceApplication = deviceServiceApplication;
     }
 
     @PostMapping
-    public ResponseEntity<DeviceDTO> createDevice(@Valid @RequestBody CreateDeviceDTO createDeviceDTO) {
-        return new ResponseEntity<>(deviceServiceApplication.createDevice(createDeviceDTO), HttpStatus.CREATED);
+    public ResponseEntity<DeviceDTO> createDeviceEnvironment(
+            @Valid @RequestBody CreateDeviceEnvDTO createDeviceEnvDTO,
+            @PathVariable Long deviceId
+    ) {
+        return ResponseEntity.ok(deviceServiceApplication.addEnvironmentVariable(deviceId, createDeviceEnvDTO));
     }
 
-    @GetMapping
-    public ResponseEntity<List<DeviceDTO>> getAllDevices() {
-        return ResponseEntity.ok(deviceServiceApplication.getAllDevices());
+    @DeleteMapping("/{envName}")
+    public ResponseEntity<DeviceDTO> deleteDeviceEnvironment(
+            @PathVariable String envName,
+            @PathVariable Long deviceId
+    ) {
+        deviceServiceApplication.deleteDeviceEnv(deviceId, envName);
+        return ResponseEntity.ok().build();
     }
 }
--- api/src/main/java/com/luiz/api/controller/DeviceTagController.java
+++ api/src/main/java/com/luiz/api/controller/DeviceTagController.java
@@ -1,14 +1,10 @@
 package com.luiz.api.controller;
 
 import com.luiz.api.service.DeviceServiceApplication;
-import com.luiz.domain.entities.device.dto.CreateDeviceDTO;
-import com.luiz.domain.entities.device.dto.CreateDeviceEnvDTO;
 import com.luiz.domain.entities.device.dto.CreateDeviceTagDTO;
 import com.luiz.domain.entities.device.dto.DeviceDTO;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,25 +12,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/device")
-public class DeviceController {
+@RequestMapping("/api/v1/device/{deviceId}/tag")
+public class DeviceTagController {
 
     private final DeviceServiceApplication deviceServiceApplication;
 
-    public DeviceController(DeviceServiceApplication deviceServiceApplication) {
+    public DeviceTagController(DeviceServiceApplication deviceServiceApplication) {
         this.deviceServiceApplication = deviceServiceApplication;
     }
 
-    @PostMapping
-    public ResponseEntity<DeviceDTO> createDevice(@Valid @RequestBody CreateDeviceDTO createDeviceDTO) {
-        return new ResponseEntity<>(deviceServiceApplication.createDevice(createDeviceDTO), HttpStatus.CREATED);
+    @DeleteMapping("/{tagName}")
+    public ResponseEntity<DeviceDTO> deleteDeviceTag(
+            @PathVariable String tagName,
+            @PathVariable Long deviceId
+    ) {
+        deviceServiceApplication.deleteDeviceTag(deviceId, tagName);
+        return ResponseEntity.ok().build();
     }
 
-    @GetMapping
-    public ResponseEntity<List<DeviceDTO>> getAllDevices() {
-        return ResponseEntity.ok(deviceServiceApplication.getAllDevices());
+    @PostMapping
+    public ResponseEntity<DeviceDTO> createDeviceTag(
+            @Valid @RequestBody CreateDeviceTagDTO createDeviceTagDTO,
+            @PathVariable Long deviceId
+    ) {
+        return ResponseEntity.ok(deviceServiceApplication.addTag(deviceId, createDeviceTagDTO));
     }
 }
